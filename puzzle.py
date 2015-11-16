ed248ff13b31f5fb619518b61f8735fa09429e5a
crabillm <michael.crabill@gmail.com>
2015-11-16 09:12:51 -0500

    can print matrix now

--- puzzle.py
+++ puzzle.py
@@ -1,1 +1,16 @@
-print 'hello world'
+def printmatrix(tmp):
+	for i in range(0, 4):
+		print
+		for j in range(0, 4):
+			print str(tmp[i][j]) + ' ',
+
+
+
+
+solved = [[0 for x in range(4)] for x in range(4)] 
+for i in range(0, 4):
+	for j in range(0, 4):
+		solved[i][j] = j + 4*i + 1
+solved[3][3] = -1
+
+printmatrix(solved)
